32843a93fd9d15a14b1de5f41be8d46ea51a1132
MuditMaurya <muditpmaurya@gmail.com>
2017-12-17 04:07:59 +0530

    Major Fix (Not Pretty)

--- Blog/blog.sh
+++ Blog/blog.sh
@@ -37,10 +37,10 @@ else
     echo "DB present"
 fi
 #Creating structure of the Table "blog" if not Exists then Create.
-blog_table="CREATE TABLE IF NOT EXISTS post (id INTEGER PRIMARY KEY AUTOINCREMENT , cat_id INTEGER, title TEXT , content TEXT);"
+blog_table='CREATE TABLE IF NOT EXISTS post (post_id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT,content TEXT,cat_id INTEGER DEFAULT "Not assigned yet");'
 echo $blog_table > /tmp/tmpblog_table
 #Creating Structure of table Table "category" if not exist then Create.
-category_table="CREATE TABLE IF NOT EXISTS category (cat_id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT );"
+category_table='CREATE TABLE IF NOT EXISTS category (cat_id INTEGER PRIMARY KEY AUTOINCREMENT,category TEXT , FOREIGN KEY(cat_id) REFERENCES post(post_id));'
 echo $category_table > /tmp/tmpcategory_table
 sqlite3 $dbname < /tmp/tmpblog_table
 sqlite3 $dbname < /tmp/tmpcategory_table
@@ -81,6 +81,7 @@ post)
         else
             #Inserting Into Table blog(DB)>post(structure)
             echo "Adding POST"
+            #if inserted successfully
             if sqlite3 $dbname "INSERT INTO post(title , content) VALUES( '$3' , '$4')";
             then
                 echo "Successfully added the post"
@@ -91,25 +92,49 @@ post)
         fi
         ;;
     list)
-        echo "Listng all the Posts"
-        #making query to list all the posts
-        LIST=`sqlite3 $dbname 'SELECT id,title,content FROM post'`;
+        #blog.sh post list > will list all the posts in the DB
+        echo "\nListng all the Posts having assigned categories\n"
+        #making query to list all the posts with assigned categories
+        LIST=`sqlite3 $dbname 'SELECT post_id,title,content,post.cat_id,category.category FROM post INNER JOIN category ON post.cat_id=category.cat_id;'`;
         #for each of the posts
-        echo -e "Post ID --> Title --> Content \n"
+        echo -e "Post ID --> Title --> Content --> Category ID --> Category \n"
         for posts in $LIST;
         do
-            #Since sqlite3 returns a pipe seperated string
-            post_id=`echo $posts | awk '{split($0,post,"|"); print post[1]}'`
-            post_title=`echo $posts | awk '{split($0,post,"|");print post[2]}'`
-            post_content=`echo $posts | awk '{split($0,post,"|");print post[3]}'`
+            echo $posts
+            #Since sqlite3 returns a pipe saparated string
+            #post_id=`echo $posts | awk '{split($0,post,"|"); print post[1]}'`
+            #post_title=`echo $posts | awk '{split($0,post,"|");print post[2]}'`
+            #post_content=`echo $posts | awk '{split($0,post,"|");print post[3]}'`
+            #post_category_id=`echo $posts | awk '{split($0,post,"|");print post[4]}'`
+            #post_category=`echo $posts | awk '{split($0,post,"|");print post[5]}'`
             #Printing the posts and Contents
-            echo -e $post_id "-->" $post_title"-->"$post_content"\n"; 
+            #echo -e $post_id " --> " $post_title" --> "$post_content" --> "$post_category_id" --> "$post_category"\n"; 
+        done
+        echo "\nListing post with Unassigned categories"
+        #making query to list all the posts with unassigned category
+        UNASSIGNED=`sqlite3 $dbname 'SELECT post_id,title,content,post.cat_id FROM post WHERE post.cat_id="Not assigned yet";'`;
+        echo -e "\nPost ID --> Title --> Content --> Category ID \n"
+        for uposts in $UNASSIGNED;
+        do
+            echo $uposts;
+            #Since sqlite3 returns a pipe saparated string
+            #upost_id=`echo $uposts | awk '{split($0,upost,"|"); print upost[1]}'`
+            #upost_title=`echo $uposts | awk '{split($0,upost,"|");print upost[2]}'`
+            #upost_content=`echo $uposts | awk '{split($0,upost,"|");print upost[3]}'`
+            #upost_category_id=`echo $uposts | awk '{split($0,upost,"|");print upost[4]}'`
+            #Printing the posts and Contents
+            #echo -e $upost_id " --> " $upost_title" --> "$upost_content" --> "$upost_category_id"\n"; 
         done
         ;;
     search)
+        #Search all the posts and 
         echo "Searching for Keyword $3"
-        search_query= 'SELECT * FROM post WHERE content LIKE %'$3'% OR title LIKE %'$3'%';
-        Search=`sqlite3 $dbname`;
+        search_query=`sqlite3 $dbname "SELECT post_id,title,content FROM post WHERE content LIKE '%$3%' OR title LIKE '%$3%';"`;
+        for searches in $search_query;
+        do
+            echo $searches;
+        done
+        #Search=`sqlite3 $dbname`;
         ;;
     * | "")
         echo "Unknown Option: $2 / empty option  \nTry --help | -h for more information"
@@ -121,8 +146,10 @@ category)
     category_flag=1
     case $2 in
     add)
+        #Assuming that there is not category mentioned with same name
+        #Adding a new category
         echo "Add category : $3"
-        if sqlite3 $dbname "INSERT INTO category (name) VALUES ('$3');"
+        if sqlite3 $dbname "INSERT INTO category (category) VALUES ('$3');"
         then
             echo "Successfully Added the Category"
         else
@@ -130,25 +157,26 @@ category)
             exit 0
         fi
         ;;
-    # Calling Category -> List
     list)
+        #Will list all the categories
         echo "List Categories"
         #making query to list all the Categories along with their ID
-        CAT_LIST=`sqlite3 $dbname 'SELECT cat_id,name FROM category'`;
+        CAT_LIST=`sqlite3 $dbname 'SELECT cat_id,category FROM category'`;
         #for each of the Categories
         echo -e "Category ID-->Name \n"
         for cats in $CAT_LIST;
         do
+            echo $cats
             #Since sqlite3 returns a pipe seperated string
-            cat_id=`echo $cats | awk '{split($0,cat,"|"); print cat[1]}'`
-            cat_name=`echo $cats | awk '{split($0,cat,"|");print cat[2]}'`
+            #cat_id=`echo $cats | awk '{split($0,cat,"|"); print cat[1]}'`
+            #cat_name=`echo $cats | awk '{split($0,cat,"|");print cat[2]}'`
             #Printing the posts and Contents
-            echo -e $cat_id "-->" $cat_name"\n"; 
+            #echo -e $cat_id "-->" $cat_name"\n"; 
         done
         ;;
     assign)
         echo "Assigning $3 post to $4 category"
-        if sqlite3 $dbname "UPDATE post SET cat_id ='$4' WHERE id='$3'";
+        if sqlite3 $dbname "UPDATE post SET cat_id=$4 WHERE post_id=$3;";
         then
             echo "Assignment Task Successfully completed"
         else
@@ -174,5 +202,4 @@ esac
 #make readme.md file
 #check returned value from DB and print null in place of them
 #search and list function not working properly need work there
-#correct database hint:Reference
 #cleaning
